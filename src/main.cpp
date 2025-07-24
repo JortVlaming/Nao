264bc1a3ab423f757d5aadf822e97920e2062b9f
JortVlaming <jort.vlaming@outlook.com>
2025-07-25 00:43:19 +0200

    second method to test

--- src/main.cpp
+++ src/main.cpp
@@ -5,8 +5,15 @@ namespace py = pybind11;
 std::string ping() {
     return "Hello c++!";
 }
+
+std::string ping2() {
+    return "Hello c++! electric boogaloo";
+}
+
+
 PYBIND11_MODULE(_nao_bindings, m) {
     m.doc() = "Example NAO lib using libqi";
 
     m.def("ping", &ping);
+    m.def("ping2", &ping2);
 }
